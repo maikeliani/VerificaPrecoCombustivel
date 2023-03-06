a2a7c87308a867ca4f6056391c94b8804ea2a646
maikeliani <maikel.iani31@gmail.com>
2023-03-05 22:08:02 -0300

    Add project files.

---
+++ VerificaPrecoCombustivel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double alcool, gasolina, price;
+
+        Console.WriteLine(" Informe o preco da gasolina");
+        gasolina = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(" Informe o preco da alcool");
+        alcool = double.Parse(Console.ReadLine());
+
+        price = alcool / gasolina;
+
+        if( price <= 0.72){
+            Console.WriteLine( " Use alcool" );
+        }
+        else
+        {
+            Console.WriteLine( " Use gasolina");
+        }
+    }
+
+}
